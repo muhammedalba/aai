03c94512d20a57c472f7f824727a4ee295e51e10
muhammedalbahle <muhammedalbahle@gmail.com>
2024-09-15 14:16:34 +0300

    first commit

---
+++ assets/js/javascribt.js
@@ -0,0 +1,34 @@
+// window.onload=async()=>{
+//     let URL="http://127.0.0.1:8000/api/Projects";
+//     let URLIMg="http://127.0.0.1:8000";
+//     await fetch(`${URL}`).then(data=>data.json())
+   
+    
+   
+//    .then(data=>{console.log(data.data);
+
+
+
+
+// // logo
+//     var imag =document.getElementById("image");
+//     var imageMobil =document.getElementById("imageMobil");
+//     var src =data.data[0].images[0];
+//     imag.src=`${URLIMg}${src}`;
+//     imageMobil.src=`${URLIMg}${src}`;
+//   }).catch(err=>console.log(err))
+ 
+    
+ 
+  
+
+// //   loading
+//   var spinners= document.getElementById("spinners").style.display="none";
+// }
+
+
+
+
+
+
+
